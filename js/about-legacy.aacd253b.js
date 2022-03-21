2d4166de858cd3651dca0b67ed5b2f4e25dd07c7
Ritu Roy <ritu.roy@dsv.com>
2022-03-21 13:39:12 +0530

    dist updated

--- js/about-legacy.aacd253b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkdemo_vuetify_app"]=self["webpackChunkdemo_vuetify_app"]||[]).push([[443],{65087:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),l={},r=(0,s.Z)(l,u,a,!1,null,null,null),i=r.exports}}]);
-//# sourceMappingURL=about-legacy.aacd253b.js.map
