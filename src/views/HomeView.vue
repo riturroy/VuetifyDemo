8797b69e5a328ce05b50af056cc0ff1a1e618d1c
Ritu Roy <ritu.roy@dsv.com>
2022-03-21 14:42:50 +0530

    added home screen update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-center">
-    <p>Home Page</p>
+  <div>
+    <h3 class="text-center">Home Page</h3>
     <div>
     <p>Sample application with below features:</p>
      <v-list-item>
@@ -16,6 +16,33 @@
      <v-list-item>
       <v-list-item-content>
         <v-list-item-title>Basic Layouts and Components</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+     <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>PWA enabled</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    
+    <p style="margin-top:32px">Features to add next: </p>
+     <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Service and API call</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+     <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Login using keycloak</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+     <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Additional components</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>Etc...</v-list-item-title>
       </v-list-item-content>
     </v-list-item>
     </div>
